d4e3a5e9555d51bdcfb024238bb4dbc23e415a61
Sean Youngs <syoungs@scottlogic.com>
2022-09-07 16:05:01 +0100

    Added additional functions to go over packages managers and paths

--- JSON dependancy filter.ps1
+++ JSON dependancy filter.ps1
@@ -1,6 +1,11 @@
-# Filter through JSON document and remove duplicate dependancies. 
+#- Filter through JSON document and remove duplicate dependancies. 
+#- Show all package managers
+#- Show all paths and packages related. 
+
+### variables ###
 $depArray = @()
-$i = 0
+$depLocation = @()
+$depPackageManager = @()
 $data = Get-content "C:\Users\syoungs\OneDrive - Scott Logic Ltd\Documents\SG-payments-dependencies.json"
 $list = $data | convertfrom-json
 #$list[0].name
@@ -9,36 +14,57 @@ foreach($bit in $list)
     #Write-Host "$($bit.name)"
     $depArray += $bit.name
 }
+$depNames = $depArray | select -Unique 
 
-$depNames = $depArray | select -Unique
-
-
-# ADDITIONAL add packager (yum or yarn?) and format into table.
-
-while($i -le $list.Length)
+# Get a procedural list of all dependancies, their versions and their package manager.
+function ShowDependancies()
 {
-        #Write-Host "$($dep)"
-    foreach($dep in $depNames)
+    $i = 0
+    
+
+    while($i -le $list.Length)
     {
-        if($dep -eq $list[$i].name)
+            #Write-Host "$($dep)"
+        foreach($dep in $depNames)
         {
-            $verResult = $list[$i].version
-            $packager = $list[$i].packager
-            Write-Host "$($dep)"
-            Write-Host ". `n            Version: $($verResult)"
-            Write-Host ".           Package Manager: $($packager)`n"
+            if($dep -eq $list[$i].name)
+            {
+                $verResult = $list[$i].version
+                $packager = $list[$i].packager
+                Write-Host "$($dep)"
+                Write-Host ". `n            Version: $($verResult)"
+                Write-Host ".           Package Manager: $($packager)`n"            
+            }
         }
-    }
-    $i++
-} 
+        $i++
+    } 
+}
 
-<#foreach($name in $depNames)
+function FilterByPacketManager()
 {
-    #write-host "$name"
-}#>
+    #
+    foreach($pm in $list)
+    {
+        #Write-Host $pm.packager
+        $global:depPackageManager += $pm.packager
+    }
+    $pManList = $depPackageManager | select -Unique
+    $pManList
+    #$depNames = $depArray | select -Unique
+
+}
+
+function FilterByPath()
+{
+    #
+}
 
 
+### Final Output ###
+#ShowDependancies
+FilterByPacketManager
 $count = $($depArray | select -Unique).count
 write-host - "`nThere are a total of $($count) unique dependancies."
 
+### Exit script ###
 Read-Host -Prompt "Press enter to exit"
