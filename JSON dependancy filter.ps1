099d1e1a023f43a2f321aab1626d0783809505b2
Sean Youngs <syoungs@scottlogic.com>
2022-09-07 11:56:49 +0100

    Fixed issue with loop not working as it was ending when unique entries ended rather than total. Added additional line showing package manager

--- JSON dependancy filter.ps1
+++ JSON dependancy filter.ps1
@@ -13,17 +13,32 @@ foreach($bit in $list)
 $depNames = $depArray | select -Unique
 
 
-# ADDITIONAL add packager (yum or yarn?) and format into table. 
-$verResult = $list[$i].version
-foreach($dep in $depNames)
+# ADDITIONAL add packager (yum or yarn?) and format into table.
+
+while($i -le $list.Length)
 {
-    if($dep -eq $list[$i].name)
+        #Write-Host "$($dep)"
+    foreach($dep in $depNames)
     {
-        
-        Write-Host "Package name: $($dep). `n            Version: $($verResult)"
+        if($dep -eq $list[$i].name)
+        {
+            $verResult = $list[$i].version
+            $packager = $list[$i].packager
+            Write-Host "$($dep)"
+            Write-Host ". `n            Version: $($verResult)"
+            Write-Host ".           Package Manager: $($packager)`n"
+        }
     }
-    $i++;
-}
+    $i++
+} 
+
+<#foreach($name in $depNames)
+{
+    #write-host "$name"
+}#>
+
 
 $count = $($depArray | select -Unique).count
-write-host "`nThere are a total of $($count) unique dependancies."
+write-host - "`nThere are a total of $($count) unique dependancies."
+
+Read-Host -Prompt "Press enter to exit"
